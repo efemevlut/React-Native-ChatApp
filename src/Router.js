95b63c846172a71bc3e2d1f88eab07c6cb2c6c74
efemevlut <efemevlut20@gmail.com>
2020-12-27 01:20:16 +0300

    Send post to firebase

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import auth from '@react-native-firebase/auth'
 
 import {Login, Sign, Timeline} from './pages';
 
 const Stack = createStackNavigator();
-// console.log(Stack)
-
 function Router() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Timeline" screenOptions={{headerShown:false}}>
+      <Stack.Navigator initialRouteName={auth().currentUser ? "Timeline" : "Login"} screenOptions={{headerShown:false}}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Sign" component={Sign} />
         <Stack.Screen name="Timeline" component={Timeline} />
--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,26 +1,51 @@
 import React, {useState} from 'react';
 import {SafeAreaView, View, Text, FlatList} from 'react-native';
-import auth from '@react-native-firebase/auth';
-import {timelinePage} from './styles';
+import database from '@react-native-firebase/database'
+import auth from '@react-native-firebase/auth' 
+import {authStyle, timelinePage} from './styles';
 import {PostItem, PostInput, Header, TopicSelectModal} from '../components';
+import moment from 'moment'
+
+const user = auth().currentUser;
 
 const Timeline = () => {
   const [topicModalFlag, setTopicModalFlag] = useState(true);
   const [selectedTopic, setSelectedTopic] = useState(null);
 
+  const selectingTopic = (value) => {
+    setSelectedTopic(value);
+    setTopicModalFlag(false);
+    
+    database().ref().on("value", (snapshot)=> {
+      console.log(snapshot.val())
+    })
+      
+  };
+
+  const sendingPost = (value) => {
+    const postObject = {
+      userEmail : user.email,
+      postText : value,
+      time : moment().toISOString()
+    }
+
+    database().ref(`${selectedTopic}/`).push(postObject);
+  }
+
+
   return (
     <SafeAreaView style={timelinePage.container}>
       <View style={timelinePage.container}>
-        <Header onTopicModalSelect={() => setTopicModalFlag(true)} title={selectedTopic}/>
+        <Header
+          onTopicModalSelect={() => setTopicModalFlag(true)}
+          title={selectedTopic}
+        />
         <FlatList data={[]} renderItem={() => null} />
-        <PostInput onSendingPost={(value) => console.log(value)} />
+        <PostInput onSendingPost={sendingPost}/>
         <TopicSelectModal
           visibility={topicModalFlag}
           onClose={() => setTopicModalFlag(selectedTopic ? false : true)}
-          onTopicSelect={(value)=>{
-            setSelectedTopic(value);
-            setTopicModalFlag(false);
-          }}
+          onTopicSelect={selectingTopic}
         />
       </View>
     </SafeAreaView>
