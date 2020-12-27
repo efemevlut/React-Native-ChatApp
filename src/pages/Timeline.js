e68125a464f6a867f211f5f93ea808e049886f7a
efemevlut <efemevlut20@gmail.com>
2020-12-27 16:13:00 +0300

    Render post item in timeline

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,37 +1,50 @@
 import React, {useState} from 'react';
 import {SafeAreaView, View, Text, FlatList} from 'react-native';
-import database from '@react-native-firebase/database'
-import auth from '@react-native-firebase/auth' 
+import database from '@react-native-firebase/database';
+import auth from '@react-native-firebase/auth';
 import {authStyle, timelinePage} from './styles';
 import {PostItem, PostInput, Header, TopicSelectModal} from '../components';
-import moment from 'moment'
+import moment from 'moment';
 
 const user = auth().currentUser;
 
 const Timeline = () => {
+  const [postList, setPostList] = useState([]);
   const [topicModalFlag, setTopicModalFlag] = useState(true);
   const [selectedTopic, setSelectedTopic] = useState(null);
 
   const selectingTopic = (value) => {
     setSelectedTopic(value);
     setTopicModalFlag(false);
-    
-    database().ref().on("value", (snapshot)=> {
-      console.log(snapshot.val())
-    })
-      
+
+    database()
+      .ref(value)
+      .on('value', (snapshot) => {
+        const data = snapshot.val();
+        const fomrmattedDate = Object.keys(data).map((key) => ({
+          userEmail: data[key].userEmail,
+          postText: data[key].postText,
+          time: data[key].time,
+        }));
+
+        fomrmattedDate.sort((a,b) => {
+          return new Date(b.time) - new Date(a.time);
+        })
+        setPostList(fomrmattedDate);
+      });
   };
 
   const sendingPost = (value) => {
     const postObject = {
-      userEmail : user.email,
-      postText : value,
-      time : moment().toISOString()
-    }
+      userEmail: user.email,
+      postText: value,
+      time: moment().toISOString(),
+    };
 
     database().ref(`${selectedTopic}/`).push(postObject);
-  }
+  };
 
+  const renderPost = ({item}) => <PostItem post={item} />;
 
   return (
     <SafeAreaView style={timelinePage.container}>
@@ -40,8 +53,12 @@ const Timeline = () => {
           onTopicModalSelect={() => setTopicModalFlag(true)}
           title={selectedTopic}
         />
-        <FlatList data={[]} renderItem={() => null} />
-        <PostInput onSendingPost={sendingPost}/>
+        <FlatList
+          keyExtractor={(_, index) => index.toString()}
+          data={postList}
+          renderItem={renderPost}
+        />
+        <PostInput onSendingPost={sendingPost} />
         <TopicSelectModal
           visibility={topicModalFlag}
           onClose={() => setTopicModalFlag(selectedTopic ? false : true)}
